83af0f5e91435d931eb364c2aada28a27b1602a5
Greg Walters <greg.gregwa@gmail.com>
2023-08-05 18:45:23 -0500

    Initial upload

---
+++ wio_terminal_LED.py
@@ -0,0 +1,19 @@
+# MicroPython / Seeed Wio Terminal / SAMD51
+# Wio-Terminal-LED.py - blink the internal blue LED
+# scruss, 2022-10
+# -*- coding: utf-8 -*-
+ 
+from machine import Pin
+from time import sleep_ms
+ 
+InternalLed=Pin(15)
+led = Pin(InternalLed, Pin.OUT)  # or Pin(15) or Pin("PA15")
+ 
+try:
+    while True:
+        led.value(not led.value())
+        sleep_ms(1200)
+except:
+    led.value(0)  # turn it off if user quits
+    exit()
+
